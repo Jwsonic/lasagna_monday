72889a3708c73e4de2f48ca1d97179d9f9b74102
John Palgut <jwsonic@gmail.com>
2018-12-26 17:37:53 -0800

    Janky web UI

--- ui/lib/ui_web/controllers/page_controller.ex
+++ ui/lib/ui_web/controllers/page_controller.ex
@@ -1,7 +1,32 @@
 defmodule UiWeb.PageController do
   use UiWeb, :controller
 
+  alias Feeder.Motor
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", token: get_csrf_token())
   end
+
+  def click(conn, params) do
+    conn
+    |> handle_turn(params)
+    |> render("index.html", token: get_csrf_token())
+  end
+
+  defp handle_turn(conn, %{"turn" => "full"}) do
+    Motor.turn_one_rotation()
+    put_flash(conn, :info, "Full turn")
+  end
+
+  defp handle_turn(conn, %{"turn" => "second"}) do
+    Motor.turn_for(1_000)
+    put_flash(conn, :info, "Turning for a second")
+  end
+
+  defp handle_turn(conn, %{"turn" => "half"}) do
+    Motor.turn_for(500)
+    put_flash(conn, :info, "Turning for a half second")
+  end
+
+  defp handle_turn(conn, _params), do: conn
 end
--- ui/lib/ui_web/router.ex
+++ ui/lib/ui_web/router.ex
@@ -2,21 +2,22 @@ defmodule UiWeb.Router do
   use UiWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", UiWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    get("/", PageController, :index)
+    post("/:turn", PageController, :click)
   end
 
   # Other scopes may use custom stacks.
